ee20c4c32cb0f3b3aae186a18597f16130aa61eb
gabriel <prates_peralta@hotmail.com>
2023-07-07 10:22:32 -0300

    fix: change return of name

--- training_app/models.py
+++ training_app/models.py
@@ -10,7 +10,7 @@ class User(models.Model):
     password = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.name
+        return self.first_name + " " + self.last_name
 
 
 class MuscleGroup(models.Model):
